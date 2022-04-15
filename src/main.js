a7d2018fd90ed0567c30e019691b8be66bcf2682
Anika <anmahaja@ucsc.edu>
2022-04-15 11:43:33 -0700

    Player control Rocket after it's fired

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
+/**
+ * Name: Anika Mahajan
+ * 
+ * Project: Rocket Patrol Mods
+ * 
+ * Date: 04/18/2022
+ * 
+ * Time Taken: 2.5 hrs 
+ * 
+ * Points Breakdown:
+ * Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
+ * Allow the player to control the Rocket after it's fired (5)
+ * Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
+ * Implement a new timing/scoring mechanism that adds time to the clock for successful hits (20)
+ * Display the time remaining (in seconds) on the screen (10)
+ */
+
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
